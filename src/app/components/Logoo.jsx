630ead68ee6d5f87c3c799c6983276e89c9449df
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-05-26 08:14:07 +0100

    changed the logo

--- src/app/components/Logoo.jsx
+++ src/app/components/Logoo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "../../../public/logo/logo2.svg";
+import Logo from "../../../public/logo/logow.svg";
 import Image from "next/image";
 const Logoo = () => {
   return (
     <div>
-      <Image src={Logo} alt="logo" />
+      <Image src={Logo} alt="logo" className="w-[2.5rem]" />
     </div>
   );
 };
