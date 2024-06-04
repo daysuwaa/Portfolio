dcf80dac0251d875581d2dea9d340a5373fa39bc
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-06-04 18:31:12 +0100

    commit

--- src/app/components/ScrollTools.jsx
+++ src/app/components/ScrollTools.jsx
@@ -9,19 +9,19 @@ import git from "../../../public/git.png";
 import github from "../../../public/github.svg";
 import vscode from "../../../public/vscode.svg";
 import vite from "../../../public/vite.png";
-import nextjswhite from "../../../public/nextjswhite.svg";
+import next from "../../../public/next.svg";
 import ToolsComponent from "./ToolsComponent";
 
 const ScrollTools = () => {
   return (
-    <div className="block md:hidden relative w-full overflow-x-scroll overflow-y-hidden">
+    <div className="block md:hidden  relative w-full overflow-x-scroll overflow-y-hidden">
       <div className="flex space-x-4 p-6 min-w-max">
         <ToolsComponent image={html} name="Html 5" />
         <ToolsComponent image={css} name="CSS 3" />
         <ToolsComponent image={js} name="Javascript" />
         <ToolsComponent image={tailwind} name="Tailwind CSS" />
         <ToolsComponent image={react} name="React JS" />
-        <ToolsComponent image={nextjswhite} name="Next JS" />
+        <ToolsComponent image={next} name="Next JS" />
         <ToolsComponent image={bootstrap} name="Bootstrap" />
         <ToolsComponent image={git} name="Git" />
         <ToolsComponent image={github} name="Github" />
