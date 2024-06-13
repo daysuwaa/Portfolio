d536cea811d1f183fac356bf618847fe62cfd075
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-06-13 19:19:19 +0100

    EDITED

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className="my-[2rem] mx-[2rem] md:mx-[5rem] tracking-wider roboto-font flex items-center justify-between">
       {/* <Logoo /> */}
-      D!
+      <p className="gwendolyn-font text-4xl">D!</p>
       <nav>
         <ul className="flex gap-6 font-semibold">
           <Link href="#section1" className="nav-link-ltr nav-link">
