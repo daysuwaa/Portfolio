a0bdb7e4c8667691eec9d6fbc5c3838c0bf01fea
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-06-21 20:05:39 +0100

    added footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="my-6">
       <hr className="my-3"></hr>
-      <h1 className="text-center inter-font font-semibold">
-        &copy; Adesuwa 2024.
+      <h1 className="text-center inter-font text-[12px] font-medium">
+        &copy; Adesuwa A. Aiwerioghene.
       </h1>
       <hr className="my-3"></hr>
     </div>
