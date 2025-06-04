f270f4c75a087fe2dd217f99b703f106c0d5b922
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-06-04 14:31:27 +0100

    uploaded my resume

--- src/app/NavPages/About.jsx
+++ src/app/NavPages/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
                 Download Resume
               </Link> */}
                 <a
-                  href="/Aiwerioghene Adesuwa Resume.pdf"
+                  href="/AdesuwaResume1.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="contactmebutton mt-5 font-medium animate-bounce"
