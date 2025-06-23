dfb7d71953428d923ea04eb55ec3fb7123b4b778
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-06-23 18:51:02 +0100

    fix: update Footer component with consistent styling and copyright year

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import { CiLinkedin } from "react-icons/ci";
+import { MdOutlineMail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { Map, MapPin, X } from "lucide-react";
+import Contactme from "../components/Contactme";
 const Footer = () => {
   return (
     <div className="my-6">
-      <hr className="my-3"></hr>
-      <h1 className="text-center inter-font text-[12px] font-medium">
-        &copy; Adesuwa A. Aiwerioghene.
+      <hr className="my-3 border-gray-300 dark:border-gray-900"></hr>
+
+      <Contactme />
+      <h1 className="text-center inter-font text-[14px] font-medium my-6">
+        &copy;2025 Adesuwa A. Aiwerioghene.
       </h1>
-      <hr className="my-3"></hr>
     </div>
   );
 };
