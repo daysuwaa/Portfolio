f8b968d861113dce2c265f96260a939ca84336b4
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-06-23 18:50:36 +0100

    feat: enhance Contact component with email form and improved layout

--- src/app/NavPages/Contact.jsx
+++ src/app/NavPages/Contact.jsx
@@ -1,54 +1,63 @@
-import React from "react";
-import { CiLinkedin, CiTwitter } from "react-icons/ci";
-import { FaXTwitter } from "react-icons/fa6";
+"use client";
+import React, { useState } from "react";
+import { CiLinkedin } from "react-icons/ci";
 import { MdOutlineMail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
-import { CiShare2 } from "react-icons/ci";
 import Link from "next/link";
+import { Map, MapPin, X } from "lucide-react";
+import { toast } from "react-hot-toast";
+import emailjs from "@emailjs/browser";
 
 const ContactInfo = [
   {
     icons: <MdOutlineMail className="w-8 h-8" />,
-    name: "Email",
+    bgcolor: "#ff9590",
     link: "mailto:adesuwaaiwerioghene12@gmail.com",
     linkName: "adesuwaaiwerioghene12@gmail.com",
   },
   {
     icons: <FaGithub className="w-8 h-8" />,
-    name: "Github",
+    bgcolor: "#a1e7cf",
     link: "https://github.com/daysuwaa",
     linkName: "daysuwaa",
   },
   {
-    icons: <CiShare2 className="w-7 h-7" />,
-    name: "Social Medias",
-    link: "https://www.linkedin.com/in/adesuwa-aiwerioghene-04538b268",
-    link2: "https://x.com/daysuwaa_a",
-    linkName: <CiLinkedin className="w-8 h-8" />,
-    linkName2: <FaXTwitter className="w-8 h-8" />,
+    icons: <MapPin className="w-7 h-7" />,
+    bgcolor: "#ffb4e6",
+    link: "",
+    linkName: "Lagos, Nigeria",
   },
 ];
 
-const ContactDisplay = ({ icons, name, link, linkName, link2, linkName2 }) => {
+const ContactDisplay = ({
+  icons,
+  bgcolor,
+  link,
+  linkName,
+  link2,
+  linkName2,
+}) => {
   return (
-    <div className="inter-font my-4 md:my-10 md:mx-auto  justify-center font-extralight text-[14px]">
-      <div className="flex items-center gap-[10px] mb-2">
-        <p className="text-fuchsia-700 dark:text-fuchsia-300 ">{icons}</p>
-        <p>{name}</p>
+    <div className="inter-font  my-4 md:my-6  font-extralight text-[14px]">
+      <div className="flex items-center gap-[10px] mb-2 ">
+        <p
+          style={{ backgroundColor: bgcolor }}
+          className="p-2 rounded-lg text-white"
+        >
+          {icons}
+        </p>
+        <Link
+          href={link}
+          className={`font-medium hover:text-rose-700 dark:hover:text-rose-200]`}
+        >
+          {linkName}
+        </Link>
       </div>
       <div className="flex gap-4">
-        {link && (
-          <Link
-            href={link}
-            className="font-medium hover:text-fuchsia-700 dark:hover:text-fuchsia-200"
-          >
-            {linkName}
-          </Link>
-        )}
         {link2 && (
           <Link
             href={link2}
-            className="font-medium hover:text-fuchsia-700 dark:hover:text-fuchsia-200"
+            className="font-medium hover:text-rose-700 dark:hover:text-rose-200"
           >
             {linkName2}
           </Link>
@@ -59,37 +68,142 @@ const ContactDisplay = ({ icons, name, link, linkName, link2, linkName2 }) => {
 };
 
 const Contact = () => {
+  const [fname, setFname] = useState("");
+  const [emaila, setEmaila] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    const toastId = toast.loading("Sending message...");
+
+    emailjs
+      .send(
+        "service_gs4zw5w",
+        "template_pzsa7dm",
+        {
+          name: fname,
+          email: emaila,
+          message: message,
+        },
+        "7N9NF5iWRaVmXltM_"
+      )
+      .then(() => {
+        toast.success("Message sent!", { id: toastId });
+        setFname("");
+        setEmaila("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("EmailJS Error:", error);
+        toast.error("Something went wrong. Try again.", { id: toastId });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
-    <section id="section4">
-      <h1 className="text-3xl text-center pompiere-font">
-        -{" "}
-        <span className="mx-3 font-inter gradienttext dark:bg-gradient-to-r from-fuchsia-400 to-[#251729]">
-          Get in Touch.
-        </span>
-      </h1>
-      <p className="text-[13px] inter-font font-medium text-center leading-6">
-        I am always open to new opportunities and collaborations. If you are
-        interested in working with me or have any questions about my projects,
-        feel free to reach out to me through any of these platforms!
-        <br></br>
-        <i className="text-fuchsia-400 font-semibold">
-          Looking forward to hearing from you!
-        </i>
-      </p>
-      <div className="grid md:grid-cols-3 items-center">
-        {ContactInfo.map((informations, index) => {
-          return (
-            <ContactDisplay
-              key={index}
-              icons={informations.icons}
-              name={informations.name}
-              link={informations.link}
-              linkName={informations.linkName}
-              link2={informations.link2}
-              linkName2={informations.linkName2}
-            />
-          );
-        })}
+    <section id="section5">
+      <div className="flex flex-col items-ceter justify-center min-h-screen w-full px-4 md:px-12 lg:px-32">
+        <div className="text-center mb-6">
+          <h2 className="text-4xl pompiere-font font-inter dark:text-white text-black mb-4">
+            Let&apos;s Work Together 🚀
+          </h2>
+          <div className="w-14 h-1 bg-gradient-to-r from-rose-400 to-pink-400 mx-auto rounded-full"></div>
+        </div>
+        <p className="text-[14px] lg:text-[16px] inter-font font-medium text-center leading-6">
+          Have a project in mind? I'd love to hear about it. Send me a message
+          and let's create something amazing together!
+          <br></br>
+          <i className="text-rose-500  leading-[40px] font-semibold">
+            Looking forward to hearing from you!
+          </i>
+        </p>
+        <div className=" grid lg:grid-cols-2 gap-6 mt-10 ">
+          <div className="w-full rounded-xl shadow-lg dark:shadow-lg  bg-gradient-to-tr from-white via-gray-50 to-white dark:from-black dark:via-[#1c1c1c] dark:to-black text-black dark:text-white transition-colors duration-500 p-5">
+            <h1 className="font-semibold text-3xl">Get in Touch ✨</h1>
+            {ContactInfo.map((informations, index) => {
+              return (
+                <ContactDisplay
+                  key={index}
+                  icons={informations.icons}
+                  bgcolor={informations.bgcolor}
+                  link={informations.link}
+                  linkName={informations.linkName}
+                  link2={informations.link2}
+                  linkName2={informations.linkName2}
+                />
+              );
+            })}
+            <h1 className="font-semibold text-2xl mb-4">Follow Me ⚡️ </h1>
+            <div className="flex items-center ">
+              <Link href="https://www.linkedin.com/in/adesuwa-aiwerioghene-04538b268?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                <CiLinkedin className=" bg-[#4c8eff] hover:bg-blue-400 text-white h-12 w-12 rounded-lg p-2 mr-4" />
+              </Link>
+              <Link href="https://x.com/daysuwaa_a?s=21&t=rDVI_vsgbDJYyKXnHKavNg">
+                <X className="bg-[#262626] hover:bg-gray-900 h-12 w-12 rounded-lg p-1 text-white" />
+              </Link>
+            </div>
+          </div>
+          {/* form */}
+          <form
+            onSubmit={handleSubmit}
+            action="https://formspree.io/f/xwpbrobj"
+            method="POST"
+            className="w-full rounded-xl shadow-lg dark:shadow-lg  bg-gradient-to-tr from-white via-gray-50 to-white dark:from-black dark:via-[#1c1c1c] dark:to-black text-black dark:text-white transition-colors duration-500 p-5"
+          >
+            <div className="mb-3">
+              <label className="mb-10">Full Name</label>
+              <input
+                type="text"
+                id="name"
+                value={fname}
+                onChange={(e) => setFname(e.target.value)}
+                placeholder="Full Name"
+                className="w-full mt-4 p-2.5 lg:p-4 placeholder:text-[14px] text-[14px] bg-[white] border-[#6c6c6c] rounded border focus:outline-none"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="mb-10">Email Address</label>
+              <input
+                type="email"
+                id="email"
+                value={emaila}
+                onChange={(e) => setEmaila(e.target.value)}
+                placeholder="Email Address"
+                className="w-full p-2.5 mt-4 lg:p-4 placeholder:text-[14px] text-[12px] bg-[white] border-[#6c6c6c] rounded border focus:outline-none"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="mb-10">Message</label>
+              <textarea
+                rows={4}
+                id="message"
+                value={message}
+                placeholder=" Tell me about your project"
+                onChange={(e) => setMessage(e.target.value)}
+                className="w-full p-2.5 mt-4 bg-[white] placeholder:text-[14px] text-black text-[14px] border-[#6c6c6c] rounded border focus:outline-none"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-full p-2.5 lg:p-4 text-[14px] font-semibold text-white rounded-lg transition
+    ${
+      loading
+        ? "bg-gray-400 cursor-not-allowed"
+        : "bg-black hover:bg-[#262626] dark:bg-rose-500/80 dark:hover:bg-rose-400/80"
+    }`}
+            >
+              {loading ? "Sending..." : "Get in Touch"}
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
--- src/app/components/Contactme.jsx
+++ src/app/components/Contactme.jsx
@@ -3,20 +3,24 @@ import React from "react";
 import { CiLinkedin } from "react-icons/ci";
 import { FaXTwitter } from "react-icons/fa6";
 import { MdOutlineMail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
 const Contactme = () => {
   return (
-    <div className="flex mt-5 gap-4 items-center cursor-pointer">
+    <div className="flex mt-5 gap-4 justify-center items-center cursor-pointer">
       <Link
         href="https://www.linkedin.com/in/adesuwa-aiwerioghene-04538b268/"
         className=" w-fit"
       >
-        <CiLinkedin className="w-[2rem]  h-[2rem] hover:text-slate-400" />
+        <CiLinkedin className=" h-8 w-8 hover:text-slate-400" />
       </Link>
       <Link href="https://x.com/daysuwaa_a" className="w-fit">
-        <FaXTwitter className="w-[1.5rem]  h-[1.8rem]  hover:text-slate-400" />
+        <FaXTwitter className=" h-8 w-8 hover:text-slate-400" />
       </Link>
       <Link href="mailto:adesuwaaiwerioghene12@gmail.com" className="w-fit">
-        <MdOutlineMail className="w-[1.8rem]  h-[2rem]  hover:text-slate-400" />
+        <MdOutlineMail className=" h-8 w-8 hover:text-slate-400" />
+      </Link>
+      <Link href="https://github.com/daysuwaa" className="w-fit">
+        <FaGithub className="h-8 w-8  hover:text-slate-400" />
       </Link>
     </div>
   );
