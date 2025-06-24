fe29606693b91f150e45c0c46457fcfa7830ff77
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-06-24 08:49:42 +0100

    refactor: adjust layout structure for improved responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -12,13 +12,15 @@ export default function Homee() {
   return (
     <div>
       <Navbar />
-      <div className="mx-[1.5rem] sm:mx-[4rem] md:mx-[4rem] lg:mx-[8rem] xl:mx-[10rem] max-w-8xl">
-        <Home />
-        <About />
-        <Tool />
-        <Projects />
-        <Projectssm />
-        <Contact />
+      <div className="flex justify-center">
+        <div className="w-full px-6 sm:px-12 md:px-12 lg:px-20 xl:px-28 max-w-7xl">
+          <Home />
+          <About />
+          <Tool />
+          <Projects />
+          <Projectssm />
+          <Contact />
+        </div>
       </div>
       <Footer />
     </div>
