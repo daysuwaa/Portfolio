ef447360b72e66d23067cad131d43eb98d6f16d5
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-08-06 11:34:33 +0100

    feat: implement ChatBubble component with dynamic script loading and updated properties

--- src/app/ChatBubble.jsx
+++ src/app/ChatBubble.jsx
@@ -1,19 +1,27 @@
 "use client";
+import { useEffect } from "react";
 
 export default function ChatBubble() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/issueiq@1.0.5/dist/loader.js";
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // clean up
+    };
+  }, []);
+
   return (
     <div
-      style={{
-        position: "fixed",
-        zIndex: 2147483647,
-        pointerEvents: "none",
-      }}
+      style={{ position: "fixed", zIndex: 2147483647, pointerEvents: "none" }}
     >
       <issueiq-bubble
-        sender-text-color="#000000"
-        bot-text-color="#fb7185cc"
-        chatbot-id="8b933d3a-532f-4778-b578-322c8405d85a"
         display-name="Adesuwa Aiwerioghene"
+        sender-text-color="#000000"
+        bot-text-color="#ffffff"
+        chatbot-id="f10e488f-ff26-48bf-8942-3fc80d5676ff"
         style={{ pointerEvents: "auto" }}
       ></issueiq-bubble>
     </div>
